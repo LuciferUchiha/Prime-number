44e57fbb941ac027ec2af84c813c8e98b2c313b7
Lucifer Uchiha <47121010+LuciferUchiha@users.noreply.github.com>
2019-12-06 14:03:48 +0100

    Update PrimeNumberTest.java

--- application/test/PrimeNumberTest.java
+++ application/test/PrimeNumberTest.java
@@ -63,4 +63,29 @@ public class PrimeNumberTest {
 		List<Long> expected = new ArrayList<Long>();
 		assertEquals(expected, PrimeNumber.getPrimeFactors(-12L));
 	}
+
+	@Test
+	public void checkAllPrimeNumbersInRange() {
+		List<Long> expected = new ArrayList<Long>();
+		expected.add(2L);
+		expected.add(3L);
+		expected.add(5L);
+		expected.add(7L);
+		expected.add(11L);
+		expected.add(13L);
+		expected.add(17L);
+		expected.add(19L);
+		expected.add(23L);
+		expected.add(29L);
+		expected.add(31L);
+		expected.add(37L);
+		expected.add(41L);
+		expected.add(43L);
+		expected.add(47L);
+		expected.add(53L);
+		expected.add(59L);
+		expected.add(61L);
+		expected.add(67L);
+		assertEquals(expected, PrimeNumber.getAllPrimeNumbersInRange(0, 70));
+	}
 }
